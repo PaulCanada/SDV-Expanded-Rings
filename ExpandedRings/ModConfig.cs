72a2f0f7b326c77a19157860cd31bcad74f7a7c0
Paul Canada <paul.canada0@gmail.com>
2018-01-21 16:26:37 -0500

    Add initial modconfig
    Added modconfig with parameter for regen amount.

---
+++ ExpandedRings/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace ExpandedRings
+{
+    class ModConfig
+    {
+        public float regenAmount { get; set; } = 0.05f;
+    }
+}
