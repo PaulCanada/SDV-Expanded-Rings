810200e10bae265d3cbc67783f4f3dae8cde9b49
Paul Canada <paul.canada0@gmail.com>
2018-01-21 16:13:21 -0500

    Initial commit of regen ring

---
+++ ExpandedRings/RegenRing.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using StardewValley;
+using StardewValley.Monsters;
+using StardewValley.Objects;
+using StardewValley.TerrainFeatures;
+
+using System;
+using System.Collections.Generic;
+
+namespace ExpandedRings
+{
+    class RegenRing : Ring
+    {
+    }
+}
