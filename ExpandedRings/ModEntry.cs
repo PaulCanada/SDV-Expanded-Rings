b13b97c9b6b048db9face734254ddfee9dc7c4a3
Paul Canada <paul.canada0@gmail.com>
2018-01-21 16:26:57 -0500

    Added initial logic for regen ring
    Added logic for regen ring to occur every second.

--- ExpandedRings/ModEntry.cs
+++ ExpandedRings/ModEntry.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 
 namespace ExpandedRings
 {
     class ModEntry : Mod
     {
+        private ModConfig config;
 
-        
+        /**
+         * Actions to perform once mod has loaded.
+         **/
         public override void Entry(IModHelper helper)
         {
-            
-            throw new NotImplementedException();
+            this.config = (ModConfig)helper.ReadConfig<ModConfig>();
+            GameEvents.OneSecondTick += GameEvents_OnSecondUpdate;
+
+        }
+
+        /**
+         * Actions to perform every second in game.
+         **/ 
+        public void GameEvents_OnSecondUpdate(object sender, EventArgs args)
+        {
+            RegenRing.healthRegen(this.config.regenAmount, Game1.player);
         }
 
 
--- ExpandedRings/RegenRing.cs
+++ ExpandedRings/RegenRing.cs
@@ -13,5 +13,28 @@ namespace ExpandedRings
 {
     class RegenRing : Ring
     {
+
+        /**
+         * Logic to handle health regeneration.
+         **/
+        public static void healthRegen(float amount, StardewValley.Farmer player)
+        {
+            if (player.leftRing.name.Equals("Ring of Regen") || player.rightRing.name.Equals("Ring of Regen"))
+            {
+                if (player.health < player.maxHealth)
+                {
+                    player.health += (int)(player.maxHealth * amount);
+
+                    if (player.health >= player.maxHealth)
+                    {
+                        player.health = player.maxHealth;
+                    }
+                }
+
+            }
+        }
+
+
+
     }
 }
